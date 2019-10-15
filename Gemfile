d31d9544996e3b9945c624ed26776fbd79cdeb97
Lucas Sandeville <lucas.sandeville@gmail.com>
2019-10-15 13:11:53 -0300

    create seed

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'devise'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'date'
+
+fulano = User.create!({ email:"fulano@talentbrand.com", password:"senha123" })
+
+date = Date.today - 15
+15.times do
+  note = Note.create!({
+    title:"#{Faker::Verb.base} #{Faker::Relationship.familial}",
+    note: Faker::Lorem.paragraph,
+    date: date,
+    priority: ["Alta", "Média", "Baixa"].sample,
+    user: fulano
+  })
+  p note
+  date += 1
+end
